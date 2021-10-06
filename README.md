f7a56804323381b134179cc2fa54a238b3efe57f
remix <http+github.com@remix.asia>
2021-10-06 09:31:42 +0900

    Radar animation

---
+++ README.md
@@ -0,0 +1,15 @@
+# Radar animation
+
+<img width="300px" src="./Radar.gif" alt="ffmpeg -i Radar.mp4 -vf scale=320:-1 -r 10 Radar.gif">
+
+<a href="Radar.V1.0.apk">Install APK</a>.
+
+## confirmed
+HTC HTL23 (Android 5.0.2)
+
+Samsung Galaxy S7 edge (Andorid 7.0)
+
+ASUS_X01AD(Android 9)
+
+## article
+https://www.remix.asia/blog/remix/2020/12/radar.html
--- app/src/main/java/asia/remix/radar/MainActivity.java
+++ app/src/main/java/asia/remix/radar/MainActivity.java
@@ -3,6 +3,9 @@ package asia.remix.radar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -10,5 +13,9 @@ public class MainActivity extends AppCompatActivity{
 	protected void onCreate( Bundle savedInstanceState ){
 		super.onCreate( savedInstanceState );
 		setContentView( R.layout.activity_main );
+
+		View radarView = (View)findViewById( R.id.radarView );
+		Animation animation= AnimationUtils.loadAnimation( this, R.anim.radar );
+		radarView.startAnimation( animation );
 	}
 }
